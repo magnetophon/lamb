37d17c1b87775a343ec61939fcc2e88321fad755
Bart Brouns <bart@magnetophon.nl>
2024-03-25 13:41:28 +0100

    pinned flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,18 @@
   description = "A flake that includes Faust";
 
   outputs = { self, nixpkgs }: {
-    # Define a development shell that includes faust and other necessary tools
-    devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [
-        nixpkgs.legacyPackages.x86_64-linux.faust2jack
+    devShell.x86_64-linux = let
+      pinnedPkgs = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/b06025f1533a.tar.gz";
+        sha256 = "sha256:1b8dim6xpcg3wyb0xa0w4h4m22npbzl2np822x4r7wiw5wnnzg5a";
+      }) {
+        system = "x86_64-linux";
+        overlays = [ ];
+        config = { };
+      };
+    in pinnedPkgs.mkShell {
+      packages = [
+        pinnedPkgs.faust2jack
       ];
     };
   };
